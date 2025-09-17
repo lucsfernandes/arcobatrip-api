ca7dfc07539f33712288a9d4884d7325c386ac94
Lucas Fernandes <lucas.fernandes@familiapires.com.br>
2025-09-17 10:21:38 -0300

    update deployment and ingress

--- src/presentation/middlewares/routes.ts
+++ src/presentation/middlewares/routes.ts
@@ -7,6 +7,11 @@ const v1RouterSwagger = {
   ...v1TripRouterSwagger
 };
 
+v1Router.get("/health", (_req, res) => {
+  // console.info('Health check is OK!');
+  res.status(200).json({ status: "ok", uptime: process.uptime() });
+});
+
 v1Router.use(v1TripRouter)
 
 export { v1Router, v1RouterSwagger };
