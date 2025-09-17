bfcb5facdb08ba04a4604dfafe35d8ab302c5695
Lucas Fernandes <lucas.fernandes@familiapires.com.br>
2025-09-17 10:21:38 -0300

    update deployment and ingress

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -8,11 +8,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/health', (_req, res) => {
-  // console.info('Health check is OK!');
-  res.status(200).json({ status: 'ok', uptime: process.uptime() });
-});
-
 app.use('/api/v1', v1Router);
 
 export default app;
