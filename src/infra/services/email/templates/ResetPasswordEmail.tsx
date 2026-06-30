a3b8fc586b8c16954529e2113332257a1b4f3711
Lucas Bandeira Fernandes <lbandeira845@gmail.com>
2026-06-30 20:49:27 -0300

    update template email

--- src/infra/services/email/templates/ResetPasswordEmail.tsx
+++ src/infra/services/email/templates/ResetPasswordEmail.tsx
@@ -71,10 +71,10 @@ const container: React.CSSProperties = {
 };
 
 const brand: React.CSSProperties = {
-  color: "#7c3aed",
+  color: "#12fda4",
   fontSize: "20px",
   fontWeight: 700,
-  margin: "0 0 24px",
+  margin: "0 0 24px"
 };
 
 const heading: React.CSSProperties = {
@@ -92,7 +92,7 @@ const paragraph: React.CSSProperties = {
 };
 
 const button: React.CSSProperties = {
-  backgroundColor: "#7c3aed",
+  backgroundColor: "#12fda4",
   borderRadius: "8px",
   color: "#ffffff",
   fontSize: "16px",
@@ -101,13 +101,13 @@ const button: React.CSSProperties = {
   textAlign: "center",
   display: "inline-block",
   padding: "12px 24px",
-  margin: "8px 0 24px",
+  margin: "8px 0 24px"
 };
 
 const link: React.CSSProperties = {
-  color: "#7c3aed",
+  color: "#12fda4",
   fontSize: "14px",
-  wordBreak: "break-all",
+  wordBreak: "break-all"
 };
 
 const footer: React.CSSProperties = {
